9beb80033d26684bf7d170e14fedb798abeee9c9
juansesu23 <juanseu3@hotmail.com>
2022-10-06 22:13:50 -0500

    acepted

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,13 +4,20 @@ import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
 //import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 //import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import ReactDOM from 'react-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faSolidFaHouse } from '@fortawesome/free-solid-svg-icons'
+
+
 
 
 const Footer = () => {
   return (
     <ContainerFooter>
-        <Link  to="home" ><Button variant="primary"> Home</Button></Link>
+        <Link  to="home" ><Button variant="primary"><FontAwesomeIcon icon={faHouse } /></Button></Link>
       
+       
+
     </ContainerFooter>
 
   )
