b262787617e1cd311497d63f453ae3c6de99bd35
juansesu23 <juanseu3@hotmail.com>
2022-10-06 21:56:19 -0500

    iconos

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { ContainerFooter } from './styles'
 import Button from 'react-bootstrap/Button';
-
 import { Link } from 'react-router-dom';
+//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+//import { faPlus } from '@fortawesome/free-solid-svg-icons';
+
 
 const Footer = () => {
   return (
     <ContainerFooter>
-        <Link  to="home" ><Button variant="primary">Home</Button></Link>
+        <Link  to="home" ><Button variant="primary"> Home</Button></Link>
+      
     </ContainerFooter>
 
   )
